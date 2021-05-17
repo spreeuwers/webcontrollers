c356588f41064f2da181fce93f7f1bd60a53ce8f
eddyspreeuwers <e.spreeuwers@gmail.com>
2021-05-17 18:10:37 +0200

    more examples, support async geHTML function

---
+++ src/FormController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by eddyspreeuwers on 5/16/21.
+ */
+/**
+ * Created by eddyspreeuwers on 5/16/21.
+ */ export class FormController {
+    init() {
+        this.getElementsByTagName('label');
+        this.firstname.value = "John";
+        this.lastname.value = "Doe";
+
+    }
+
+}
+//# sourceMappingURL=FormController.js.map
