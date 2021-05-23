2917051121fe2796b92e0a52d37351cfbe37d820
eddyspreeuwers <e.spreeuwers@gmail.com>
2021-05-23 19:41:26 +0200

    fixed ts typing

--- src/FormController.ts
+++ src/FormController.ts
@@ -6,13 +6,13 @@
 
 export class FormController {
 
-    public firstname: string;
-    public lastname: string;
+    public firstname: HTMLFormElement;
+    public lastname: HTMLFormElement;
 
     init(){
        //this.getElementsByTagName('label');
-       this.firstname.value = "John";
-       this.lastname.value = "Doe";
+        this.firstname.value = "John";
+        this.lastname.value = "Doe";
     }
 
 }
